fb9c46f35afcfa5621d5ecb567150b2bcef12038
Andrew Southard <andrewsouthard@users.noreply.github.com>
2025-03-24 08:56:08 -0400

    Add foundations for updater (#11)

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,14 @@
 import { useEffect } from "react";
 import Home from "./screens/Home";
 import { setupLogging } from "./lib/logging";
+import { checkForUpdates } from "./lib/updater";
 import "./App.css";
 
 export default function App() {
   // Forward all console logs to persisted logs
   useEffect(() => {
     setupLogging();
+    checkForUpdates();
   }, []);
   return (
     <div className="flex flex-col h-screen bg-gray-50 relative">
